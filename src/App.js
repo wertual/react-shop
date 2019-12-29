963f386358652ece99f26b5a124c3abe4d40601e
Vitaliy Rubeli <vitalikrubeli@gmail.com>
2019-12-29 22:29:47 +0200

    finishing sign in and sign up

--- src/App.js
+++ src/App.js
@@ -33,8 +33,6 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          }, () => {
-            console.log(this.state);
           })
         });
 
